ccfa17c824a4a811388a95f8442d035bce36bbaf
dkcvivek <vivek.jha@dkcexport.co.in>
2026-01-06 10:35:25 +0530

    Deleted commented code from auth.ts

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,17 +1,3 @@
-// import { AuthResponse } from "../types/auth";
-// import { apiFetch } from "./api";
-
-// export function login(email: string, password: string) {
-//   return apiFetch<AuthResponse>("/auth/login/", {
-//     method: "POST",
-//     body: JSON.stringify({ email, password }),
-//   });
-// }
-
-// export function logout() {
-//   return apiFetch("/auth/logout/", { method: "POST" });
-// }
-
 export const loginWithEmail = async (email: string, password: string) => {
   const res = await fetch("http://128.100.10.210:8000/login/", {
     method: "POST",
